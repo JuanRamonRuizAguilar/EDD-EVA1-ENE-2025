bdba0f1b5c0d16269845df1dfb8f0b5aed927ec2
Juan Ramon Ruiz <159494661+JuanRamonRuizAguilar@users.noreply.github.com>
2025-01-30 17:41:05 -0600

    Add files via upload

---
+++ EVA1_1_MANEJO_MEMORIA/src/main/java/com/mycompany/eva1_1_manejo_memoria/EVA1_1_MANEJO_MEMORIA.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_1_manejo_memoria;
+
+/**
+ *
+ * @author Dell
+ */
+public class EVA1_1_MANEJO_MEMORIA {
+
+    public static void main(String[] args) {
+        System.out.println("INICIA MAIN");
+        A();
+            System.out.println("TERMINA MAIN ");
+     
+    }
+    public static void A(){
+        System.out.println("INICIA A");
+        B();
+            System.out.println("TERMINA A");
+    }
+    public static void B(){
+        System.out.println("INICIA B");
+            System.out.println("TERMINA B");
+             }
+     }
+            
